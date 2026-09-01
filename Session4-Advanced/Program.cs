0e899b885e1f2fb99f3e44b02bd69d1d76584d30
Ahmed Zakaria <ahmed.zakaria10az@gmail.com>
2026-09-01 13:20:36 +0200

    Exercise 1: Student Grade Manager

--- Session4-Advanced/Program.cs
+++ Session4-Advanced/Program.cs
@@ -4,7 +4,62 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Exercise 1: Student Grade Manager
+            //1. Create a Collection with these grades: 85, 92, 78, 95, 88, 70, 100, 65
+            //List<int> grades = new List<int> {88,95,72,97,60,75,99,61};
+
+            //2. Print the collection, Count, first and last grade
+            //Console.WriteLine("Grades:");
+            //foreach (int grade in grades)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+
+            //Console.WriteLine($"Count:{grades.Count}");
+            //Console.WriteLine($"First grade:{grades[0]}");
+            //Console.WriteLine($"Last grade:{grades[^1]}");
+
+            //3. Sort the grades ascending, then print
+            //grades.Sort();
+
+            //Console.WriteLine("Grades:");
+            //foreach (int grade in grades)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+
+            //4. Get the first grade above 90
+            //int firstAbove90 = grades.Find(grade => grade > 90);
+            //Console.WriteLine($"{firstAbove90}");
+
+            //5. Get all grades below 75 (failing grades)
+            //List<int> failingGrades = grades.FindAll(grade => grade < 75);
+
+            //Console.WriteLine("Failing grades:");
+            //foreach (int grade in failingGrades)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+
+            //6. Remove all failing grades (below 75)
+            //grades.RemoveAll(grade => grade < 75);
+
+            //7. Check if any grade equals 100
+            //bool gradeAbove100 = grades.Exists(grade => grade == 100);
+            //Console.WriteLine($"{gradeAbove100}");
+
+            //8. Create a List<string> where each grade becomes "Grade: X"
+            //List<string> gradeStrings = new List<string>();
+
+            //foreach (int grade in grades)
+            //{
+            //    gradeStrings.Add($"Grade: {grade}");
+            //}
+            //foreach (string grade in gradeStrings)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+            #endregion
         }
     }
 }
